3a2b3a7a01c783cf6f91ec1294b5995def782b61
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-10-04 13:26:32 +0200

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,14 +1,5 @@
-def hashtags(thedic):
-    print("# = 1 Percent")
-    biggest, smallest = biggest_smallest(thedic)
-    for k,v in thedic.items():
-        value = (v * 100) / total       
-        if v == biggest:
-            print(k, round(value)*'#', '<-- biggest value')
-        elif v == smallest:
-            print(k, round(value)*'#', '<-- smallest value')  
-        else:
-            print(k, round(value)*'#')
+#Calculating the smallest and biggest value in a dictionary.
+#Returning the biggest and smallest in a tuples.
 
 def biggest_smallest(thedic):
     biggest = 0
@@ -21,18 +12,35 @@ def biggest_smallest(thedic):
             smallest = v
     return biggest, smallest
 
+#Printing the stripdiagram for each key and value in the dictionary.
+def hashtags(thedic):
+    print("# = 1 Percent")
+    biggest, smallest = biggest_smallest(thedic)
+
+    #Giving note of the biggest and smallest value.
+    for k,v in thedic.items():    
+        value = (v * 100) / total       
+        if v == biggest:
+            print(k, round(value)*'#', '<-- biggest value')
+        elif v == smallest:
+            print(k, round(value)*'#', '<-- smallest value')  
+        else:
+            print(k, round(value)*'#')
+
 newfile = 'y'
 print("Starting script2.py\n")
 while newfile == 'y':
     file = open(input("Open file: "))
     print("")
+
+    #Starting values
     total = 0
     biggest = 0
     file_type = "Nucleotides"
-
     DNA = ['A', 'T', 'C', 'G']
     aminoacid_dic = {'A':0, 'R':0, 'N':0, 'D':0, 'C':0, 'E':0, 'Q':0, 'G':0, 'H':0, 'I':0, 'L':0, 'K':0, 'M':0, 'F':0, 'P':0, 'S':0, 'T':0, 'W':0, 'Y':0, 'V':0}
 
+    #Reading file
     for line in file:
         line = line.strip()
         if not line.startswith(">"):
@@ -48,6 +56,8 @@ while newfile == 'y':
     print('This file contains a total of', total, file_type)
 
     if file_type == "Nucleotides":
+
+        #Making new dictionary for Nucleotides.
         nuc_dic = {'A':aminoacid_dic['A'], 'C':aminoacid_dic['C'], 'T':aminoacid_dic['T'],'G':aminoacid_dic['G']} 
         print('Counter:', nuc_dic)
         G = nuc_dic['G']
@@ -55,13 +65,18 @@ while newfile == 'y':
         C = nuc_dic['C']
         A = nuc_dic['A']
 
-        nuc_dic.values()
+        #Caluclating the G-C percentage.
         x = (G + C) / (A + T + G + C) * 100
+
         print('G-C Percentage:',round(x),'% (rounded)')
+
+        #Calling functions with the nucleotide dictionary as input.
         hashtags(nuc_dic)
 
     if file_type == "Aminoacids":
         print('Counter:',aminoacid_dic)
+
+        #Calling functions with the aminoacid dictionary as input.
         hashtags(aminoacid_dic)
     
 
