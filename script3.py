f055c8190048fc65389ee9ad66de53199bc7ece5
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-10-04 14:40:59 +0200

    Add files via upload

---
+++ script3.py
@@ -0,0 +1,11 @@
+# Source : https://www.ncbi.nlm.nih.gov/protein/?term=Haemophilia+A+factor+Viii+human
+
+file = open('sequence_DNA.fasta')
+newfile = open('new.fasta', 'w')
+
+for line in file:
+    line = line.strip()
+    print(line, file=newfile)
+
+file.close()
+newfile.close()
