7e0930a89e31467469deaa23bc5050cd81d0bf3d
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2021-10-07 17:11:50 +0200

    Update script4.py
    Ik heb dit ervan kunnen maken maar kom er maar niet uit hoe je bij de verschillende lines komt zodat je de letter in 1 line kan veranderen. Ben bang dat ik niet veel verder kom, ben benieuwd of jij er al wat meer duidelijkheid in hebt

--- script4.py
+++ script4.py
@@ -1,14 +1,18 @@
-# Source: https://www.ncbi.nlm.nih.gov/gene/3043
-input_filename = open("sequenceProtSCA.fasta")
+ Source: https://www.ncbi.nlm.nih.gov/gene/3043
+input_filename = open("sequenceprot.fasta.fasta")
 output_filename = input("Please choose your output file name: ")
-input_letter = input('What is your input letter? ')
-output_letter = input('What is your output letter? ')
+replacing_letter = input('What is your output letter? ')
+spot = input("What is the position of your input letter? ")
 output_filename = open(output_filename, "w")
+# Replaces one letter with the other, but you can not choose a line yet.
+def replacer(replaced_lines, letter, position):
+        return replaced_lines[:position] + letter + replaced_lines[position+1:]
 
 for lines in input_filename:
     lines = lines.rstrip()
     if not lines.startswith(">"):
-        lines = lines.replace(input_letter, output_letter, 1)
-    output_filename.write()
+        print(replacer(lines, replacing_letter, int(spot)))
+        output_filename.write(lines)
+
 input_filename.close()
-# AAa
+output_filename.close()
