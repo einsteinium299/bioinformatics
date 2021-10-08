4599c0a4277cca4c49e79a31bb69c3efe728ff15
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-10-08 14:15:54 +0200

    Update script5.py

--- script5.py
+++ script5.py
@@ -1,1 +1,40 @@
+file = open('/homes/sldehaan/Documents/Practicum/Opdracht01/seq2.fasta', 'r')
+output_file = open('outfile.fasta', 'w')
 
+string = ''
+aminoacid = ''
+
+dic_P = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+for line in file:
+    line = line.strip()
+    if not line.startswith('>'):       
+        for char in line:
+            string += char
+            if len(string) == 3:
+                aminoacid += dic_P[string]
+                string = ''
+                if len(aminoacid) == 50:
+                    output_file.write(aminoacid + '\n')
+                    aminoacid = ''
+               
+
+
+file.close()
+output_file.close()
