e9c9cd07704c48038ba87d3acd6354ec5e31fae9
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-10-27 23:23:58 +0200

    Partial piece of script7

--- script7.py
+++ script7.py
@@ -1,1 +1,28 @@
+genbank_file = open('sequence_DNA.gb')
+dict_sections = {}
+list_sections = []
+section = ''
+startline = True
 
+for line in genbank_file:
+    section = ''
+    startline = True
+    line = line.rstrip()
+    if not line.startswith(' '):
+        for letter in line:            
+            section += letter
+            if startline == True:
+                if letter == ' ':       
+                    list_sections.append(section)
+                    section = ''
+                    startline = False
+        
+        section = section.strip()            
+        print(section)
+
+for section2 in list_sections:
+    dict_sections[section2] = ''
+
+print(list_sections)
+print('')
+print(dict_sections)
