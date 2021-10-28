0916390a1648c357f7b7a5331e3a3e1dad69aeff
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-10-28 15:48:27 +0200

    Update script8.py

--- script8.py
+++ script8.py
@@ -1,1 +1,27 @@
+def counting_atoms():
+    myfile = open('mmdb_5UAK.pdb', 'r')
 
+    dic_atom_weight = {
+        'C ':12.011,
+        'N ':14.007,
+        'O ':15.999,
+        'S ':32.06,
+    }
+
+    outfile = open('output.pdb', 'w')
+    atom = ''
+    lst = []
+    count = 0
+    for line in myfile:
+        if line.startswith('ATOM'):
+            atom = line[77:79]
+            print(atom)
+            count += dic_atom_weight[atom]
+    print(count)
+    
+    myfile.close()
+    outfile.close()
+
+def main():
+    counting_atoms()
+main()
