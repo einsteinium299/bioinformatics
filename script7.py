83fb29d7b30f0e98d427ef51db9af8c5781632af
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-10-28 07:15:10 +0200

    Removing multiple whitespace in string

--- script7.py
+++ script7.py
@@ -1,22 +1,32 @@
 genbank_file = open('sequence_DNA.gb')
 dict_sections = {}
+whitespace_count = 0
+
 
 for line in genbank_file:
     section = ''
     startline = True
     keyword = ''
+    whitespace_count = 0
 
     line = line.rstrip()
     if not line.startswith(' '):
         for letter in line:            
             section += letter
+            if letter == ' ':
+                whitespace_count += 1
+                
             if startline == True:
                 if letter == ' ':
                     keyword = section.strip()
                     section = ''
                     startline = False
         
-        section = section.strip() 
+
+        #removing multiple whitespace from string                
+        section = " ".join(section.split())
+        
         dict_sections[keyword] = section            
 
 print(dict_sections)
+print(whitespace_count)
