f229bd57543a316bf983e3039becf0e51c224f4a
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-11-01 15:52:56 +0100

    Update eindopdracht.py

--- eindopdracht.py
+++ eindopdracht.py
@@ -2,51 +2,85 @@ def input_user():
     input_file = open('sequence.gb', 'r')
     return input_file
 
+
+def origin_seq(sequence):
+    sequence = sequence[1::]
+    final_seq = ''
+
+    for line in sequence:
+        line = line.split()
+        for split in line:
+            if not split.isdigit():
+                final_seq += split
+
 def reading_file(input_file):
     origen_loop = False
     features_loop = False
+    subsection = False
 
     sequence = []
-    final_seq = ''
+    features_list = []
+
 
     for line in input_file:
         # print(line, end='')
-        line.strip()
         
         if line.startswith('FEATURES'):
             features_loop = True
+
         elif line.startswith('ORIGIN'):
             origen_loop = True
             features_loop = False
+        
         elif line.startswith('//'):
             origen_loop = False
 
-        while features_loop == True:
-            print(line, end='')
-            break
 
-        while origen_loop == True:
+
+        if features_loop == True:
+            if not line[5:].startswith(' ') or subsection == True:
+                # print(line.strip())
+                features_list.append(line.strip())
+
+                if not line[5:].startswith(' '):
+                    subsection = True
+                else:
+                    subsection = False
+
+        if origen_loop == True:
             sequence.append(line)
-            break
 
-    for line in sequence:
-        line = line.strip().replace(' ', '')
+    return sequence, features_list
 
-        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+def printing_terminal_output(sequence, features_list):
+    new_list = []
 
-        for num in nums:
-            line = line.replace(str(num), '')
+    features_list = features_list[1::]
+    # print(len(features_list))
 
-        if line == 'ORIGIN':
-            line = ''
-        else:
-            final_seq += line
+    new_line = ''
+    count = 0
 
-    # print(final_seq)
+    for line in features_list:
+        # print(line)
+        new_line += line
+        count += 1
+        if count == 2:
+            new_list.append(new_line)
+            count = 0
+            new_line = ''
+
+    for line in new_list:
+        print(line)
+
 
 def main():
     input_file = input_user()
 
-    reading_file(input_file)
-main()
+    sequence, features_list = reading_file(input_file)
+    
+    origin_seq(sequence)
 
+    printing_terminal_output(sequence, features_list)
+
+main()
